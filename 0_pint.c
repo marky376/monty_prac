eaaacc00e7dfbfd16a65fa620f7cbedb2f660df8
WisdomNjoroge <mercyn936@gmail.com>
2023-12-14 18:15:28 +0300

    Monty language

---
+++ 0_pint.c
@@ -0,0 +1,19 @@
+#include "MONTY_H"
+/**
+ * f_pint - prints the value at the top of the stack
+ * @head: stack head
+ * @counter: line number
+ * Return: NULL
+*/
+void f_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
--- monty.h
+++ monty.h
@@ -36,4 +36,5 @@ typedef struct instruction_s
 	        char *opcode;
 		        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+void f_pint(stack_t **head, unsigned int counter);
 #endif
